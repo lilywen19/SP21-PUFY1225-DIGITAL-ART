115c365a23abf6eb21654266ab0f61cc7502622f
lilywen19 <xuw632@newschool.edu>
2021-04-05 08:22:43 -0700

    update/p5js code file

---
+++ js/painting.js
@@ -0,0 +1,39 @@
+function setup(){
+	createCanvas(600, 500);
+
+}
+
+
+function draw(){
+	background(226, 213, 198);
+	noStroke();
+	// first collumn
+	// yellow
+	fill(213, 178, 62);
+	rect(10, 20, 60, 60);
+	// pink
+	fill(201, 133, 145);
+	rect(30, 90, 80, 70);
+	// yellow
+	fill(213, 178, 62);
+	rect(15, 180, 75, 70);
+	// blue
+	fill(129, 164, 184);
+	rect(40, 265, 65, 60);
+	// pink
+	fill(201, 133, 145);
+	rect(30, 335, 70, 60);
+	// yellow
+	fill(213, 178, 62);
+	rect(40, 490, 60, 10);
+	// second collumn
+	// pink
+	fill(201, 133, 145);
+	rect(130, 10, 80, 60);
+	// blue
+	fill(129, 164, 184);
+	rect(135, 85, 65, 60);
+	
+
+
+}
